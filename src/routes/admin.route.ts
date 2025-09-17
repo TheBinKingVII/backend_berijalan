df42edc7b4f24993ecc986bbbf986f8af29b9878
BINTORO <147918033+TheBinKingVII@users.noreply.github.com>
2025-09-17 19:53:13 +0700

    Delete src directory

--- src/routes/admin.route.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-
-const adminRouter = Router();
-
-// TODO: Add admin routes here
-
-export default adminRouter;
--- src/routes/auth.route.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-
-const authRouter = Router();
-
-// TODO: Add auth routes here
-
-export default authRouter;
